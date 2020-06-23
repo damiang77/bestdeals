a06a5779c5044e936122bfe1051d52e107aade2a
Damian Garbera <damian@pc44.home>
2020-06-23 21:34:25 +0200

    build

---
+++ build/precache-manifest.ecc4f65d539bd824d06dd0f811e0ddc4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ac54a40a2f0a7d7007eee0506745291",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d898b054b7ac9864ac36",
+    "url": "/static/css/2.0ac97ba3.chunk.css"
+  },
+  {
+    "revision": "6f1e58bfda893917b247",
+    "url": "/static/css/main.6cf4a5e5.chunk.css"
+  },
+  {
+    "revision": "d898b054b7ac9864ac36",
+    "url": "/static/js/2.03ba2a6d.chunk.js"
+  },
+  {
+    "revision": "6f1e58bfda893917b247",
+    "url": "/static/js/main.14195f03.chunk.js"
+  },
+  {
+    "revision": "5fffdc6a1c76fcf26260",
+    "url": "/static/js/runtime~main.3c36303a.js"
+  },
+  {
+    "revision": "316f477b075448301ee77bd3c3b9a846",
+    "url": "/static/media/comment.316f477b.svg"
+  },
+  {
+    "revision": "72a57ac36097b16414be9140cc3639ea",
+    "url": "/static/media/link.72a57ac3.svg"
+  },
+  {
+    "revision": "32cdef1bb41305abb5f64f227bac2838",
+    "url": "/static/media/login-form.32cdef1b.png"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  }
+]);
